784e024e57415d07d5f10af885a24b14bc596470
Geronimo <rafaelgeronimofx@gmail.com>
2024-04-03 23:05:07 -0300

    iniciando a estilização da tabela pelo cabeçalho

--- pass-in-web/src/components/attendee-list.tsx
+++ pass-in-web/src/components/attendee-list.tsx
@@ -16,49 +16,51 @@ export function AttendeeList() {
         </div>
       </div>
 
-      <table>
-        <thead>
-          <tr>
-            <th>
-              <input type='checkbox' />
-            </th>
-            <th>Código</th>
-            <th>Participante</th>
-            <th>Data de inscrição</th>
-            <th>Data do check-in</th>
-            <th></th>
-          </tr>
-        </thead>
+      <div className='border border-white/10 rounded-lg'>
+        <table className='w-full border'>
+          <thead>
+            <tr className='border-b border-white/10'>
+              <th className='py-3 px-2.5 text-sm font-semibold'>
+                <input type='checkbox' />
+              </th>
+              <th className='py-3 px-2.5 text-sm font-semibold'>Código</th>
+              <th className='py-3 px-2.5 text-sm font-semibold'>Participante</th>
+              <th className='py-3 px-2.5 text-sm font-semibold'>Data de inscrição</th>
+              <th className='py-3 px-2.5 text-sm font-semibold'>Data do check-in</th>
+              <th className='py-3 px-2.5 text-sm font-semibold'></th>
+            </tr>
+          </thead>
 
-        <tbody>
-          <tr>
-            <td>
-              <input type='checkbox' />
-            </td>
-            <td>3145135</td>
-            <td>
-              <div>
-                <span>Rafael Geronimo</span>
-                <span>rafael.geronimo@gmail.com</span>
-              </div>
-            </td>
-            <td>7 dias atrás</td>
-            <td>3 dias atrás</td>
-            <td></td>
-          </tr>
-        </tbody>
+          <tbody>
+            <tr>
+              <td>
+                <input type='checkbox' />
+              </td>
+              <td>3145135</td>
+              <td>
+                <div>
+                  <span>Rafael Geronimo</span>
+                  <span>rafael.geronimo@gmail.com</span>
+                </div>
+              </td>
+              <td>7 dias atrás</td>
+              <td>3 dias atrás</td>
+              <td></td>
+            </tr>
+          </tbody>
 
-        <tfoot>
-          <tr>
-            <td colSpan={3}>
-              Mostrando 10 de 228 itens
-            </td>
-            <td colSpan={3}>
-              Página 1 de 23
-            </td>
-          </tr>
-        </tfoot>
-      </table>
+          <tfoot>
+            <tr>
+              <td colSpan={3}>
+                Mostrando 10 de 228 itens
+              </td>
+              <td colSpan={3}>
+                Página 1 de 23
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </div>
   )
 }
